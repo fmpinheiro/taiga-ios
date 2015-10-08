fc3e625a88386d8f5958138faf75d08362f3861c
Rhonan Carneiro <rhonan@Rhonans-MacBook-Pro.local>
2015-10-08 20:44:47 -0300

    Creating project...

---
+++ taiga-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  taiga-ios
+//
+//  Created by Rhonan Carneiro on 08/10/15.
+//  Copyright © 2015 Taiga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ taiga-iosTests/taiga_iosTests.swift
@@ -0,0 +1,36 @@
+//
+//  taiga_iosTests.swift
+//  taiga-iosTests
+//
+//  Created by Rhonan Carneiro on 08/10/15.
+//  Copyright © 2015 Taiga. All rights reserved.
+//
+
+import XCTest
+@testable import taiga_ios
+
+class taiga_iosTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
